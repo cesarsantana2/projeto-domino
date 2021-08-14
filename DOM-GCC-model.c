c1b8e9a9c82a00e8f4dceaf0a82b98c207ec451f
cesar santana <cjsseng@gmail.com>
2021-08-14 15:03:34 -0300

    Atualizando as estruturas peca e conjunto_de_pecas

--- DOM-GCC-model.c
+++ DOM-GCC-model.c
@@ -2,35 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//variáveis usadas em todas as funções
-int i = 0;
-int x = 0;
-int pc1 = 0;
-int pc2 = 0;
-int n = 7;
-
-//parametrando funções
-void embaralhapecas();
-void montapecas();
-void mostrapecas();
-
-//estrutura base para as peças
-struct cria_peças{
-    int lado1;
-    int lado2;
-    char stat;
+//Estrutura de uma Peca
+struct Peca {
+    int valores [2];
 };
-struct cria_peças peças[28];
 
-//estutura da mesa que será usada posteriormente
-struct mostrar_mesa{
-    int ladoE;
-    int ladoD;
+//Estrutura de um conjunto de peças
+struct conjunto_de_pecas  {
+    Peca pecas[28];
 };
-struct mostrar_mesa mesa[28];
-
-//estrutura auxiliar para embaralhamento
-struct rand_auxiliar{
-    int aux;
-};
-struct rand_auxiliar auxiliar;
