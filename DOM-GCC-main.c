7b7dade45e525957390655be8af7f7dcc08d8669
cesar santana <cjsseng@gmail.com>
2021-08-15 11:11:50 -0300

    Atualizando a main

--- DOM-GCC-main.c
+++ DOM-GCC-main.c
@@ -1,3 +1,15 @@
+/*
+PROJETO DA DISCIPLINA DE LABORATÓRIO DE PROGRAMACAO
+
+--- JOGO DE DOMINO ---
+
+AUTORES: CAIO ENGLER
+         CESAR SANTANA
+         GABRIEL CAVALCANTI
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "DOM-GCC-controller.c"
